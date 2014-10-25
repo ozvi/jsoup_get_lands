6aa8de48dadce03d18774e5dc3ef335661c9d653
ozvi <ozvi.inc@gmail.com>
2014-10-25 20:35:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main{
 	public static void main(String[] args) {
 		System.out.println("itzik is cool2");
 		System.out.println("itzik is cool2");
+		System.out.println("itzik is cool2");
+		System.out.println("itzik is cool2");
+		System.out.println("itzik is cool2");
 		
 	}
 	
